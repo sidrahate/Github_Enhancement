b45893032cfd71dd46eeac2964e7ed83585d7684
Siddhesh <srahate123@gmail.com>
2020-03-25 18:07:10 +0530

    Title (Upto 60 char): Initial Commit
    Lan ID : 12345678
    
    Description (Upto 120 Char) : Initial commit after Creating Project

---
+++ src/Main/Simple.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello my Name is Siddhesh Rahate");
+	}
+
+}
